2a6fe472d5222e3912ab655b6c3e4d9840a1bfec
kict4329 <kict4329@naver.com>
2020-10-02 23:55:12 +0900

    hw2 ing

--- Lab6/lab06_main.cpp
+++ Lab6/lab06_main.cpp
@@ -29,14 +29,9 @@ double phi = 45;
 double r = 13;
 double x, y, z;
 double cam_uv = 1; // camera up vector (0, cam_uv, 0)을 결정
-bool theta_direction_isReverse = false;
 
 int main(int argc, char** argv)
 {
-	int submenu1;
-	int submenu_color;
-	int submenu_figure;
-
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowSize(500, 500);
@@ -67,7 +62,7 @@ int main(int argc, char** argv)
 /*초기화 함수*/
 void init()
 {	// 배경색 설정
-	glClearColor(0.2f, 0.5f, 0.8f, 1.0f); // dark blue
+	glClearColor(0.2f, 0.5f, 0.8f, 1.0f);
 }
 
 void draw()
@@ -147,11 +142,9 @@ void specialKeyboard(int key, int x, int y)
 		break;
 	case GLUT_KEY_UP:
 		theta -= 5.3;
-		theta_direction_isReverse = true;
 		break;
 	case GLUT_KEY_DOWN:
 		theta += 5.3;
-		theta_direction_isReverse = false;
 		break;
 	}
 
